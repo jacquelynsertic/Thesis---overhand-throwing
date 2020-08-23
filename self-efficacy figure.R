d8188aae2e04ecf256fbd93f69b6bba9e82d7a6a
jacquelynsertic <jacquelyn.sertic23@gmail.com>
2020-08-23 12:11:03 -0500

    Added a boxplot to each group with 'order'.
    Adjusted the widths.
    
    Changed the X- and Y- labels.

--- self-efficacy figure.R
+++ self-efficacy figure.R
@@ -11,25 +11,26 @@ se <- read_excel('C:\\Users\\jacqu\\Documents\\las vegas\\unlv\\thesis\\Data Col
 
 # Boxplot with dotplot
 
-se %>% ggplot(aes(x = blocks, y = score)) + #creates the ggplot
-  #geom_boxplot(alpha = 0, width = .8) + #creates boxplot, alpha sets transparency, width adjusts spaces between boxplots
-  #stat_boxplot(geom = "errorbar", width = 0.2) + #creates error bars with specified width for whiskers
+se %>% ggplot(aes(x = blocks, y = score, order = factor(Group))) + #creates the ggplot
+  geom_boxplot(alpha = 0, #transparent boxplot
+    width = .8) + #adjusts spaces between boxplots
+  # stat_boxplot(geom = "errorbar", width = 0.2) + #creates error bars with specified width for whiskers
   geom_dotplot( #creates dotplot
     binaxis = "y", #puts the dots along the y-axis
     stackdir = "center", #centerst the dots
     stackratio = 1, #stacks the dots
     binpositions = "all", #shows dots of the groups
-    position = position_dodge(width = .65), #adjustst the spacing of the dots between the groups
+    position = position_dodge(width = .785), #adjustst the spacing of the dots between the groups
     dotsize = .85, #size of the dots
     aes(fill = Group)) + #allows the groups to be represented by the dots
   scale_fill_manual(values = c("black", "grey")) + #colors the dots with specified colors
-  scale_y_continuous(name="Mean Score / Participant", #names the y-axis
+  scale_y_continuous(name="Scores", #names the y-axis
     breaks = c(0:10), #adds tick marks for each value
     limit = c(0,10)) + #set the min and max values for y-axis
   stat_summary(fun = mean, #creates a dot at the mean
     aes(group = Group), #creates a mean value for each group
     size = .85, #changes mean dot size,
-    position = position_dodge(width =1.4), #moves the mean dot away from the midline
+    position = position_dodge(width = 1.3), #moves the mean dot away from the midline
     color = "black", #changes the mean dot color
     fill = "white",
     shape = 21, #shape = 21 creates a circle
@@ -41,9 +42,9 @@ se %>% ggplot(aes(x = blocks, y = score)) + #creates the ggplot
     stroke = 1.5) + #boarder thickness
   guides(fill = guide_legend(order = 1), size = guide_legend(order = 2)) + 
   guides(size = guide_legend(override.aes = list(alpha = 1))) + #overrides legend transparency
-  xlab("Blocks") + #labels x-axis
+ # xlab("Blocks") + #labels x-axis
   ggtitle("Self-Efficacy Scores") + #labels title
   theme_fivethirtyeight() + 
-  theme(axis.title = element_text()) + 
-  ylab('Mean Score / Participant') +
-  theme(legend.title=element_blank())  #removes legend titles
+  theme(axis.title = element_text()) +
+  theme(legend.title=element_blank()) +  #removes legend titles
+  theme(axis.title.x = element_blank()) #removes x-axis label
--- throwing figure.R
+++ throwing figure.R
@@ -10,19 +10,20 @@ throwing <- read_excel('C:\\Users\\jacqu\\Documents\\las vegas\\unlv\\thesis\\Da
 
 # dotplot
   
-throwing %>% ggplot(aes(x = block, y = score)) + #creates the ggplot
- # geom_boxplot(alpha = 0, width = .8) + #creates boxplot, alpha sets transparency, width adjusts spaces between boxplots
- # stat_boxplot(geom = "errorbar", width = 0.2) + #creates error bars with specified width for whiskers
+throwing %>% ggplot(aes(x = block, y = score, order = factor(Group))) + #creates the ggplot
+  geom_boxplot(alpha = 0, #transparent boxplot
+    width = .8) + #adjusts spaces between boxplots
+  # stat_boxplot(geom = "errorbar", width = 0.2) + #creates error bars with specified width for whiskers
   geom_dotplot( #creates dotplot
     binaxis = "y", #puts the dots along the y-axis
     stackdir = "center",#centers the dots
     stackratio = 1, #stacks the dots
     binpositions = "all", #shows dots of the groups
-    position = position_dodge(width = .7), #adjustst the spacing of the dots between the groups
+    position = position_dodge(width = .785), #adjustst the spacing of the dots between the groups
     dotsize = .85, #size of the dots
     aes(fill = Group)) + #allows the groups to be represented by the dots
   scale_fill_manual(values = c("black", "grey")) + #colors the dots with specified colors
-  scale_y_continuous(name="Mean Score / Participant", #names the y-axis
+  scale_y_continuous(name="Scores", #names the y-axis
     breaks = c(0:8), #adds tick marks for each value
     limit = c(0,8)) +  #set the min and max values for y-axis
   stat_summary(fun = mean,  #creates a dot at the mean
@@ -40,12 +41,10 @@ throwing %>% ggplot(aes(x = block, y = score)) + #creates the ggplot
     stroke = 1.5) + #boarder thickness
   guides(fill = guide_legend(order = 1), size = guide_legend(order = 2)) +  
   guides(size = guide_legend(override.aes = list(alpha = 1))) + #overrides legend transparency
-  xlab("Blocks") + #labels x-axis
-  ggtitle("Self-Efficacy Scores") + #labels title
+ # xlab("Blocks") + #labels x-axis
+  ggtitle("Throwing Scores") + #labels title
   theme_fivethirtyeight() + 
-  theme(axis.title = element_text()) + 
-  ylab('Mean Score / Participant') +
-  theme(legend.title=element_blank())  #removes legend titles
-
-
+  theme(axis.title = element_text()) +
+  theme(legend.title=element_blank()) +  #removes legend titles
+  theme(axis.title.x = element_blank()) #removes x-axis label
 
